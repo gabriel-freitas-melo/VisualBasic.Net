44e58a5d93d25083857495d1f7328f6d262bf1dd
gabriel-freitas-melo <gabrielfreitas1813@gmail.com>
2023-03-17 17:34:45 -0300

    Aprendendo e atualizando o funcionamento do primeiro programa

---
+++ Primeiro_Programa/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Esse é o primeiro Programa VB")
+        Dim num1, num2, num3 As Integer
+        num1 = 45
+        num2 = 5
+        num3 = num1 + num2
+        Console.WriteLine(num3)
+    End Sub
+End Module
