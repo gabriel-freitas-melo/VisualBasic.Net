c192aa8eb6243e08d13fbf530794ddb613844bb5
gabriel-freitas-melo <gabrielfreitas1813@gmail.com>
2023-03-20 10:52:28 -0300

    Consertando pequenos erros no read.me e nos tipos de dados

---
+++ Tipo_de_dados/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+
+Module Program
+    Sub Main(args As String())
+        Dim salarioMensal, salarioAnual As Double
+        Dim meses As Integer
+        salarioMensal = 1500.0
+        meses = 18
+        salarioAnual = salarioMensal * meses
+        'Cadeia de caracteres de formato composto
+        Console.Writeline("Salario anual: {0:C}", salarioAnual)
+        'Cadeia de caracteres de formato interpolado
+        Console.WriteLine("Salário Anual: {salarioAnual}")
+    End Sub
+End Module
