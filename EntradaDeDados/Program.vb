bf426de5fd2884fd8101b934353ca1b85c0d8656
gabriel-freitas-melo <gabrielfreitas1813@gmail.com>
2023-03-20 11:58:23 -0300

    Trabalhando com entrada de dados

---
+++ EntradaDeDados/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Resgistros do usuário:")
+        Dim nome As String
+        Dim idade As Integer
+        Console.Writeline("Digite seu nome:")
+        nome = Console.ReadLine()
+        Console.WriteLine("Digite a sua idade:")
+        idade = Console.ReadLine()
+        Console.WriteLine($"Olá {nome}, você tem {idade} anos")
+        Console.WriteLine("Digite enter para sair do programa")
+        Console.ReadLine()
+        Console.WriteLine("Até logo")
+
+    End Sub
+End Module
